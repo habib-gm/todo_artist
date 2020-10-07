6db142c8494ccd95a25e13774bf4e512eea9d4f3
Ahmet Ümit BAYRAM <38078662+ahmetumitbayram@users.noreply.github.com>
2020-10-07 13:13:47 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
