049fa81ca89a8448c7b25a19c9eb3117298d2bd9
Ahmet Ümit BAYRAM <38078662+ahmetumitbayram@users.noreply.github.com>
2020-10-07 13:20:13 +0300

    Update urls.py

--- todo_artist/urls.py
+++ todo_artist/urls.py
@@ -1,4 +1,4 @@
-"""todo_drf URL Configuration
+"""todo_artist URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
